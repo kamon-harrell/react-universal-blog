75a4270b2b34a760e75e596b18f06c7816d3123f
Kamon <harrellkamon@gmail.com>
2016-11-26 14:26:03 +1300

    basic setup working, routing issues fixed

--- app-client.js
+++ app-client.js
@@ -1,19 +1,18 @@
-// app-client.js
-import React from 'react'
-import { render } from 'react-dom'
-import { Router } from 'react-router'
-import createBrowserHistory from 'history/lib/createBrowserHistory'
-const history = createBrowserHistory()
+  // app-client.js
+  import React from 'react'
+  import { render } from 'react-dom'
+  import { Router, browserHistory } from 'react-router'
 
-// Routes
+  const history = (browserHistory)
 
-import routes from './routes'
+  // Routes
+  import routes from './routes'
 
-const Routes = (
-  <Router history={history}>
-    { routes }
-  </Router>
-)
+  const Routes = (
+    <Router history={history}>
+      { routes }
+    </Router>
+  )
 
-const app = document.getElementById('app')
-render(Routes.app)
+  const app = document.getElementById('app')
+  render(Routes, app)
